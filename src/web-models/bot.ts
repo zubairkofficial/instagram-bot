efeabe7468e7c2c4dddef4f0f8cbef91a35418a5
MuzmmilKhan1 <muzmmil.khan16@gmail.com>
2024-03-19 15:15:13 +0500

    --

--- src/web-models/bot.ts
+++ src/web-models/bot.ts
@@ -47,10 +47,10 @@ export class Bot extends PuppeteerStarter {
             await this
                 .page
                 .frames()
-                .find(frame => frame.url.toString().startsWith("https://www.google.com/recaptcha"))
+                .find(frame => frame.url().toString().startsWith("https://www.google.com/recaptcha"))
                 ?.$(".recaptcha-checkbox");
             await recaptchaCheckbox?.click();
-            console.log(this.page.frames().map(frame => frame.url.toString()));
+            console.log(this.page.frames().map(frame => frame.url().toString()));
 
         await setTimeout(1500);
         await this.page.screenshot({ path: 'screenshot.jpg' });
